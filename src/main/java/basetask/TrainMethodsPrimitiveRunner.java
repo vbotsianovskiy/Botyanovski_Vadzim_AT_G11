c2b2f2988ca395117fe39d08f31fc325eca3d550
vbaty <v.sokol_91@mail.ru>
2024-11-17 05:26:19 +0300

    add homework_day1

---
+++ src/main/java/basetask/TrainMethodsPrimitiveRunner.java
@@ -0,0 +1,15 @@
+package main.java.basetask;
+
+public class TrainMethodsPrimitiveRunner {
+    public static void main(String[] args) {
+        TrainMethodsPrimitive zet = new TrainMethodsPrimitive();
+        zet.printInt(7);
+        zet.printLong(1153551424);
+        zet.printChar('$');
+        zet.printFloat(554.77f);
+        zet.printDouble(56.12);
+        zet.printShort((short) 200);
+        zet.printByte((byte) 120);
+        zet.printBoolean(true);
+    }
+}
